690baf2076408124415d71210494f7bf5102b708
Liheng Cao <lihengcao@nyu.edu>
2021-07-07 10:55:50 -0400

    python test

--- srcpy/imu_core_cpp.cpp
+++ srcpy/imu_core_cpp.cpp
@@ -38,14 +38,14 @@ public:
 PYBIND11_MODULE(imu_core_cpp, m){
   py::class_<ImuMsg>(m, "ImuMsg")
     .def(py::init<>())
-    // .def("") // deconstructor
+    // .def("") // destructor
     .def("is_valid", &ImuMsg::is_valid)
     .def("reply_debug_string", &ImuMsg::reply_debug_string)
     .def("command_debug_string", &ImuMsg::command_debug_string);
 
   py::class_<ImuInterface, PyImuInterface /* trampoline class */>(m, "ImuInterface")
     .def(py::init<const std::string>())
-    // .def("") // deconstructor
+    // .def("") // destructor
     .def("initialize", &ImuInterface::initialize)
     .def("get_acceleration", &ImuInterface::get_acceleration)
     .def("get_angular_rate", &ImuInterface::get_angular_rate)
@@ -54,7 +54,7 @@ PYBIND11_MODULE(imu_core_cpp, m){
 
   py::class_<Imu3DM_GX3_25, ImuInterface>(m, "Imu3DM_GX3_25")
     .def(py::init<const std::string , const bool&>(), "port_name"_a, "stream_data"_a=false)
-    // .def() // deconstructor
+    // .def() // destructor
     .def("initialize", &Imu3DM_GX3_25::initialize)
     .def("open_usb_port", &Imu3DM_GX3_25::open_usb_port, "bauderate"_a)
     .def("receive_message", &Imu3DM_GX3_25::receive_message, "msg"_a, "stream_mode"_a)
--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,32 @@
-# WIP, just unplug and replug in imu if errors occur when exiting process
-
 import imu_core.imu_core_cpp, time, numpy as np, signal, sys
 
-np.set_printoptions(precision=3)
 
+def handle_signal(s, f):
+    global keep_running
+    keep_running = False
+
+
+keep_running = True
 imu = imu_core.imu_core_cpp.Imu3DM_GX3_25("/dev/ttyACM1", True)  # assuming port of /dev/ttyACM1
 
+# imu.reset_device()
 imu.initialize()
-
-def handle_signal(signal_, frame_):
-    #   TODO
-    #   stop_reading_loop();
-    #   while(!stop_streaming_data()){}
-    #   reset_device();
-    #   usb_stream_.close_device();
-    sys.exit(0)
-
 signal.signal(signal.SIGINT, handle_signal)
+while keep_running:
+    # print("acc: ", [f"{e:+.3f}" for e in imu.get_acceleration()], "ang rate: ", [f"{e:+.3f}" for e in imu.get_angular_rate()])
+    print("acc = [", end='')
 
-while True:
-    print("Accleration: ", imu.get_acceleration(), "Angular Rate: ", imu.get_angular_rate())
+    for e in imu.get_acceleration():
+        print(f"{e:+.3f} ", end='')
+
+    print("]  ang rate = [", end='')
+
+    for e in imu.get_angular_rate():
+        print(f"{e:+.3f} ", end='')
+
+    print("]")
+
     time.sleep(0.05)
+
+del imu  # explicitly call destructor
+sys.exit(0)
